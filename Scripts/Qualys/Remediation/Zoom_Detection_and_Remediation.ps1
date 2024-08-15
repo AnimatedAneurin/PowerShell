0cbaf41fae96380ede00361bb32d0ba567ed0f14
AnimatedAneurin <aneurin.weale2@outlook.com>
2024-08-15 09:59:23 +0100

    First Upload

---
+++ Scripts/Qualys/Remediation/Zoom_Detection_and_Remediation.ps1
@@ -0,0 +1,66 @@
+﻿<#
+    .SYNOPSIS
+        Zoom Detection and Remediation
+
+    .DESCRIPTION
+        This script will detect and then remove Zoom from the users appdata and the HKEY_USERS Registry.
+        This is NOT an Uninstall.
+
+    .NOTES
+        Name: Zoom_Detection_and_Remediation.ps1
+        Version: 1.0
+        Author: Aneurin Weale - VAR
+        Date Created: 14/08/2024
+        Last Updated: 14/08/2024
+        URL: 
+#>
+
+#Region Detection
+$userRoot = "C:\Users"
+$UserList = Get-ChildItem -Path $userRoot -Directory -Force -ErrorAction SilentlyContinue
+foreach ($User in $UserList) {
+    #Write-Host "Checking $userRoot\$($User.Name)"
+    if (Test-Path -Path "$userRoot\$($User.Name)\AppData\Roaming\Zoom") {
+        Write-Host "Application Found Under Username: $($User.Name)"
+        Write-Host $TRUE
+    }
+    <# Do not uncomment this section. This will break detection for Configuration Items on SCCM.
+    else {
+        Write-Host "Application Not Found"
+        Write-Host $FALSE
+    }#>
+}
+#EndRegion
+
+#Region Removal
+$userRoot = "C:\Users"
+$UserList = Get-ChildItem -Path $userRoot -Directory -Force -ErrorAction SilentlyContinue
+$HKUReg = New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+$HKURoot = "HKU:\"
+$HKUList = Get-ChildItem -Path $HKURoot -Force -ErrorAction SilentlyContinue
+foreach ($User in $UserList) {
+    if (Test-Path -Path "$userRoot\$($User.Name)\AppData\Roaming\Zoom") {
+        Remove-Item -Recurse -Path "$($User.FullName)\AppData\Roaming\Zoom"
+        #Start-Process -FilePath "$userRoot\$($User.Name)\AppData\Roaming\Zoom\uninstall\Installer.exe" -ArgumentList "/Uninstall" -Wait
+        if (!(Test-Path -Path "$userRoot\$($User.Name)\AppData\Roaming\Zoom")) {
+            Write-Host "Application Removed"
+            foreach ($HKU in $HKUList) {
+                #Write-Host "Checking $HKURoot\$($HKU.Name)"
+                if (Test-Path -Path "$HKURoot\$($HKU.Name)\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZoomUMX") {
+                    #Write-Host "Found $HKURoot\$($HKU.Name)\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZoomUMX"
+                    Remove-Item -Recurse -Path "$HKURoot\$($HKU.Name)\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZoomUMX"
+                    if (!(Test-Path -Path "$HKURoot\$($HKU.Name)\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZoomUMX")) {
+                        Remove-PSDrive -Name HKU
+                        Write-Host "Registry Cleaned"
+                    } else {
+                        Remove-PSDrive -Name HKU
+                        Write-Host "Failed to Clean"
+                    }
+                }
+            }
+        } else {
+            Write-Host "Failed to Uninstall"
+        }
+    }
+}
+#EndRegion
