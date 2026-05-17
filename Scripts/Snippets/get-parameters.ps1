c1083e6bbc796356eddaaf9103aff77765f2abca
AnimatedAneurin <aneurin.weale2@outlook.com>
2026-05-17 23:57:25 +0100

    Added info on WhatIf

--- Scripts/Snippets/get-parameters.ps1
+++ Scripts/Snippets/get-parameters.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding()] # CmdletBinding allows the script use built-in PowerShell arguments such as verbose.
+[CmdletBinding(SupportsShouldProcess)] # CmdletBinding allows the script use built-in PowerShell arguments such as verbose. SupportsShouldProcess allows the script to run in WhatIf mode.
 param(
     [ValidateRange(1, 12)]
     [int]$Month, # Integer Parameter that validates user input ensuring that value provided is in range of 1 to 12.
@@ -11,3 +11,19 @@ param(
 
     [switch]$Computers # Switch Parameter that acts as a true or false condition
 )
+
+#! If using SupportsShouldProcess and WhatIf, it would be important to know that any destructive code must run inside a .shouldprocess() block. Otherwise, the destructive code may run regardless.
+
+#** An example of how destructive code should be wrapped:
+
+$folder = "C:\DLM"
+if ($PSCmdlet.ShouldProcess($folder, "Remove Folder")) {
+    Remove-Item -Path $folder -Recurse -Force
+}
+
+#** If you want the script to run in WhatIf mode but want specific code inside the script to run normally, then you need to append the -WhatIf:$false parameter to the command, as shown below:
+
+Start-Transcript -Path "C:\DLM\TestLog.log" -WhatIf:$false
+Stop-Transcript -ErrorAction SilentlyContinue
+
+#** Please note that not all cmdlets support the -WhatIf parameter so you may need to see if it's available using: Get-Command [cmdlet] -Syntax
